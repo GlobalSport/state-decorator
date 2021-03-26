37b1ffbb06c966d4ffab877903637a65b2aa8ed9
Damien Garbarino <d.garbarino@mycoachsport.com>
2021-03-26 09:06:43 +0100

    feat: add state sharing sample

---
+++ examples/src/SliceView.tsx
@@ -0,0 +1,112 @@
+import React, { Fragment } from 'react';
+
+import { useStoreSlice } from '../../dist';
+
+import FlashingBox from './FlashingBox';
+import { store } from './Slice';
+
+import Box from '@material-ui/core/Box';
+import Button from '@material-ui/core/Button';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+
+import { makeStyles } from '@material-ui/core/styles';
+
+function Buttons() {
+  const { actions } = useStoreSlice(store, ({ actions }) => ({ actions }));
+
+  return (
+    <FlashingBox>
+      <Box>
+        <Button variant="outlined" onClick={() => actions.setValue1('test1')}>
+          Set Value 1 to "test1"
+        </Button>
+        <Button variant="outlined" onClick={() => actions.setValue2('test2')}>
+          Set Value 2 to "test2"
+        </Button>
+      </Box>
+    </FlashingBox>
+  );
+}
+
+type ValueProps = {
+  isLoading: boolean;
+  value: string;
+  title: string;
+};
+
+function Value(p: ValueProps) {
+  const { value, title, isLoading } = p;
+
+  if (isLoading) {
+    return (
+      <FlashingBox>
+        <CircularProgress />
+      </FlashingBox>
+    );
+  }
+
+  return (
+    <FlashingBox>
+      <Typography>
+        {title}: {value}
+      </Typography>
+    </FlashingBox>
+  );
+}
+
+function Value1() {
+  const { value, isLoading } = useStoreSlice(store, ({ s, isLoading }) => ({
+    value: s.value1,
+    isLoading: isLoading('setValue1'),
+  }));
+
+  return <Value title="Value 1" value={value} isLoading={isLoading} />;
+}
+
+function Value2() {
+  const { value, isLoading } = useStoreSlice(store, ({ s, isLoading }) => ({
+    value: s.value2,
+    isLoading: isLoading('setValue2'),
+  }));
+
+  return <Value title="Value 1" value={value} isLoading={isLoading} />;
+}
+
+function SliceView() {
+  const styles = useStyles({});
+  return (
+    <Box mb={2} p={2} className={styles.box}>
+      <Typography variant="h6">Slices</Typography>
+      <Typography variant="caption">
+        Each component is using only a slice of the state and is refreshed only if slice changed (flashing when is
+        rendered)
+      </Typography>
+      <Box mt={1}>
+        <Buttons />
+        <Grid container>
+          <Grid item md={6} xs={12}>
+            <Value1 />
+          </Grid>
+          <Grid item md={6} xs={12}>
+            <Value2 />
+          </Grid>
+        </Grid>
+      </Box>
+    </Box>
+  );
+}
+
+const useStyles = makeStyles((theme) => ({
+  btn: {
+    marginRight: theme.spacing(2),
+  },
+
+  box: {
+    border: '1px solid #ccc',
+    borderRadius: 5,
+  },
+}));
+
+export default SliceView;
--- examples/src/commons.tsx
+++ examples/src/commons.tsx
@@ -1,9 +1,5 @@
 import React, { useEffect, useRef } from 'react';
 
-type FlashingBoxProps = {
-  children: any;
-};
-
 const styles: React.CSSProperties = {
   border: `1px solid`,
   padding: 20,
@@ -11,22 +7,3 @@ const styles: React.CSSProperties = {
   outline: '2px solid transparent',
   transition: `all ease .2s`,
 };
-
-export function FlashingBox(p: FlashingBoxProps) {
-  const countRef = useRef(0);
-  countRef.current++;
-  const node = useRef<HTMLDivElement>();
-
-  useEffect(() => {
-    node.current.style.outline = '2px solid red';
-    setTimeout(() => {
-      node.current.style.outline = styles.outline.toString();
-    }, 500);
-  }, [countRef.current]);
-
-  return (
-    <div ref={node} style={styles}>
-      {p.children}
-    </div>
-  );
-}
