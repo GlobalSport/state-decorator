242b31049b3ada0d622212a282ca689b84723b0e
Damien Garbarino <d.garbarino@mycoachsport.com>
2022-07-27 16:34:41 +0200

    fix(helpers): return partial state

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,89 +1,85 @@
 /**
  * Effect function that sets the first action argument to the specified prop.
  */
-export function setArgIn<S, K extends keyof S>(propName: K): (ctx: { s: S; args: [S[K]] }) => S {
+export function setArgIn<S, K extends keyof S>(propName: K): (ctx: { s: S; args: [S[K]] }) => Partial<S> {
   return ({ s, args: [v] }) =>
     s[propName] === v
       ? null
-      : {
-          ...s,
+      : (({
           [propName]: v,
-        };
+        } as unknown) as Partial<S>);
 }
 
 /**
  * Effect function that sets <code>true</code> in the specified prop.
  */
-export function setTrueIn<S, K extends keyof S>(propName: K): (ctx: { s: S }) => S {
+export function setTrueIn<S, K extends keyof S>(propName: K): (ctx: { s: S }) => Partial<S> {
   return ({ s }) =>
     s[propName]
       ? null
-      : {
-          ...s,
+      : (({
           [propName]: true,
-        };
+        } as unknown) as Partial<S>);
 }
 
 /**
  * Effect function that sets <code>false</code> in the specified prop.
  */
-export function setFalseIn<S, K extends keyof S>(propName: K): (ctx: { s: S }) => S {
+export function setFalseIn<S, K extends keyof S>(propName: K): (ctx: { s: S }) => Partial<S> {
   return ({ s }) =>
     !s[propName]
       ? null
-      : {
-          ...s,
+      : (({
           [propName]: false,
-        };
+        } as unknown) as Partial<S>);
 }
 
 /**
  * Effect function that sets toggles the boolean value of the specified prop.
  */
-export function toggleProp<S, K extends keyof S>(propName: K): (ctx: { s: S }) => S {
-  return ({ s }) => ({
-    ...s,
-    [propName]: !s[propName],
-  });
+export function toggleProp<S, K extends keyof S>(propName: K): (ctx: { s: S }) => Partial<S> {
+  return ({ s }) =>
+    (({
+      [propName]: !s[propName],
+    } as unknown) as Partial<S>);
 }
 
 /**
  * Effect function that sets the update the specifed prop as an array using first action argument as index
  * and second as value to set.
  */
-export function setMapItem<S, K extends keyof S, T>(propName: K): (ctx: { s: S; args: [string, T] }) => S {
-  return ({ s, args: [id, v] }) => ({
-    ...s,
-    [propName]: {
-      ...s[propName],
-      [id]: v,
-    },
-  });
+export function setMapItem<S, K extends keyof S, T>(propName: K): (ctx: { s: S; args: [string, T] }) => Partial<S> {
+  return ({ s, args: [id, v] }) =>
+    (({
+      [propName]: {
+        ...s[propName],
+        [id]: v,
+      },
+    } as unknown) as Partial<S>);
 }
 
 /**
  * Effect function that sets the update the specifed prop as an array using first action argument as index
  * and second as value to set.
  */
-export function setArrayItem<S, K extends keyof S, T>(propName: K): (ctx: { s: S; args: [number, T] }) => S {
+export function setArrayItem<S, K extends keyof S, T>(propName: K): (ctx: { s: S; args: [number, T] }) => Partial<S> {
   return ({ s, args: [idx, v] }) => {
     const arr = (s[propName] as any) as T[];
 
     const newArr = [...arr];
     newArr[idx] = v;
-    return {
-      ...s,
+    return ({
       [propName]: newArr,
-    };
+    } as unknown) as Partial<S>;
   };
 }
 
 /**
  * Effect function that sets the asynchronous action result to the specified prop.
  */
-export function setResIn<S, K extends keyof S>(propName: K): (ctx: { s: S; res: S[K] }) => S {
-  return ({ s, res }) => ({
-    ...s,
-    [propName]: res,
-  });
+export function setResIn<S, K extends keyof S>(propName: K): (ctx: { s: S; res: S[K] }) => Partial<S> {
+  return ({ res }) =>
+    (({
+      [propName]: res,
+    } as unknown) as Partial<S>);
 }
