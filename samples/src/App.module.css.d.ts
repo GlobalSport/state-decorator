ab75396b73bfb98e71d965c5f75e4e7d2538f5a8
Damien Garbarino <d.garbarino@mycoachsport.com>
2023-08-28 07:39:12 +0200

    chore: update deps

--- samples/src/App.module.css.d.ts
+++ samples/src/App.module.css.d.ts
@@ -1,5 +1,4 @@
-import globalClassNames from './style.d';
-declare const classNames: typeof globalClassNames & {
+declare const classNames: {
   readonly root: 'root';
   readonly paper: 'paper';
   readonly tabs: 'tabs';
